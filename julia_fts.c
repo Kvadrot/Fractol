3f5f61ccc6df2cfdc4954bdf3b7020b1f773d3df
Kvadrot <send.me.your.maild@gmail.com>
2024-07-17 14:32:43 +0200

    julia_fts.c norms 95%, TODO: toLONG line to solve

--- julia_fts.c
+++ julia_fts.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 13:39:10 by itykhono          #+#    #+#             */
-/*   Updated: 2024/07/17 14:11:01 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/07/17 14:30:51 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,59 @@
 
 int	julia(double real, double comp, double real_num, double complex_num)
 {
-    int n;
+	double	z_real_temp;
+	int		n;
 
 	n = 0;
 	while (real * real + comp * comp <= 4 && n < MAX_ITER)
-    {
-        double z_real_temp = real * real - comp * comp + real_num;
-        comp = 2 * real * comp + complex_num;
-        real = z_real_temp;
-        n++;
-    }
-    return n;
+	{
+		z_real_temp = real * real - comp * comp + real_num;
+		comp = 2 * real * comp + complex_num;
+		real = z_real_temp;
+		n++;
+	}
+	return (n);
 }
 
-void draw_julia(main_obj *obj, double real_num, double complex_num)
+int	drawhelper(main_obj *obj, int iter, int x, int y)
 {
-    int x, y, color;
-    for (y = 0; y < WIN_HEIGHT; y++)
-    {
-        for (x = 0; x < WIN_WIDTH; x++)
-        {
-            double real = obj->math_num.real_num.min + (x + obj->math_num.x_offset) * (obj->math_num.real_num.max - obj->math_num.real_num.min) / WIN_WIDTH;
-            double imag = obj->math_num.imag_num.min + (y + obj->math_num.y_offset) * (obj->math_num.imag_num.max - obj->math_num.imag_num.min) / WIN_HEIGHT;
-            int n = julia(real, imag, real_num, complex_num);
+	int	color;
+	int	brightness;
+	int	pixel_index;
 
-            // Map the number of iterations to a color
-    		if (n == MAX_ITER)
-            {
-                color = 0x000000; // Black for points inside the set
-            }
-            else
-            {
-                int brightness = 255 * n / MAX_ITER;
-                color = (brightness << 16) + (brightness << 8) + brightness; // Grayscale
-            }
+	if (iter == MAX_ITER)
+		color = 0x000000;
+	else
+	{
+		brightness = 255 * iter / MAX_ITER;
+		color = (brightness << 16) + (brightness << 8) + brightness;
+	}
+	pixel_index = (y * obj->sl1) + (x * obj->bpp1 / 8);
+	obj->img_data[pixel_index] = color;
+	obj->img_data[pixel_index + 1] = (color >> 8);
+	obj->img_data[pixel_index + 2] = (color >> 16);
+}
 
-            int pixel_index = (y * obj->sl1) + (x * obj->bpp1 / 8);
-            obj->img_data[pixel_index] = color;
-            obj->img_data[pixel_index + 1] = (color >> 8);
-            obj->img_data[pixel_index + 2] = (color >> 16);
-        }
-    }
+void	draw_julia(main_obj *obj, double real_num, double complex_num)
+{
+	int		x;
+	int		y;
+	int		n;
+	double	real;
+	double	imag;
+
+	y = 0;
+	while (y < WIN_HEIGHT)
+	{
+		x = 0;
+		while (x < WIN_WIDTH)
+		{
+			real = obj->math_num.real_num.min + (x + obj->math_num.x_offset) * (obj->math_num.real_num.max - obj->math_num.real_num.min) / WIN_WIDTH;
+			imag = obj->math_num.imag_num.min + (y + obj->math_num.y_offset) * (obj->math_num.imag_num.max - obj->math_num.imag_num.min) / WIN_HEIGHT;
+			n = julia(real, imag, real_num, complex_num);
+			drawhelper(obj, n, x, y);
+			x++;
+		}
+		y++;
+	}
 }
